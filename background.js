517baac3bd7734e7439252e1aafb79f4f7c63636
sam2024 <sahusam2024@gmail.com>
2024-09-13 00:02:49 +0530

    wobb extension
    production

---
+++ background.js
@@ -0,0 +1,181 @@
+let popupWindowId = null;
+let message = false;
+let instagramURL = "";
+
+function getInstagramPageType(url) {
+  const instagramProfilePattern = /^https:\/\/www\.instagram\.com\/[^\/]+\/?$/;
+  const instagramUrlPattern = /^https:\/\/www\.instagram\.com\/.*$/;
+
+  if (instagramProfilePattern.test(url)) {
+    return 'profile';
+  } else if (instagramUrlPattern.test(url)) {
+    return 'instagramPage';
+  } else {
+    return 'other';
+  }
+}
+
+
+function getCurrentTabUrl() {
+  return new Promise((resolve, reject) => {
+    const queryInfo = { active: true, currentWindow: true };
+    const api = typeof browser !== "undefined" ? browser : chrome;
+    if (api.tabs && api.tabs.query.length === 1) {
+      api.tabs
+        .query(queryInfo)
+        .then((tabs) => resolve(tabs[0].url))
+        .catch((error) => reject(error));
+    } else {
+      api.tabs.query(queryInfo, (tabs) => {
+        if (api.runtime.lastError) {
+          reject(api.runtime.lastError);
+        } else {
+          resolve(tabs[0].url);
+        }
+      });
+    }
+  });
+}
+
+
+chrome.action.onClicked.addListener(function () {
+  console.log(popupWindowId)
+  if (popupWindowId !== null) {
+    chrome.windows.get(popupWindowId, (window) => {
+      if (chrome.runtime.lastError) {
+        console.log("Error getting window:", chrome.runtime.lastError.message);
+        popupWindowId = null;
+        createPopup();
+      } else {
+        if (window.state === "minimized") {
+          chrome.windows.update(popupWindowId, { state: "normal" });
+        } else {
+          chrome.windows.update(popupWindowId, { focused: true });
+        }
+      }
+    });
+  } else {
+    if (!popupWindowId) {
+      createPopup();
+    }
+  }
+});
+
+
+function getSocialNameFromLink(link) {
+  try {
+    let socialName = "";
+    if (typeof link == "string") {
+      if (link.includes(".com")) {
+        let array = link.split("/");
+
+        array.forEach((el, index) => {
+          if (el.includes(".com")) {
+            socialName = array[index + 1].split("?")[0];
+          }
+        });
+      } else if (link.includes("@")) {
+        socialName = link.replace("@", "");
+      } else {
+        socialName = link;
+      }
+
+      socialName = socialName.toLowerCase();
+      return socialName;
+    } else {
+      return "invalidSocialName";
+    }
+  } catch (e) {
+    return "invalidSocialName";
+  }
+}
+function createPopup() {
+  chrome.windows.getCurrent(function (currentWindow) {
+    const width = Math.round(currentWindow.width * 0.4); 
+    const height = currentWindow.height - 100;
+    const left = currentWindow.width - width; 
+    const top = 90;
+    getCurrentTabUrl().then(async (currentUrl) => {
+      const urlOfType = getInstagramPageType(currentUrl);
+      if (urlOfType === "other" || urlOfType === "instagramPage") {
+        const [tab] = await chrome.tabs.query({
+          active: true,
+          currentWindow: true,
+        });
+
+        const [result] = await chrome.scripting.executeScript({
+          target: { tabId: tab.id },
+          func: () => {
+            return window.messageScriptLoaded || false;  
+          },
+        });
+    
+        if (tab.id !== undefined) {
+          if (!result.result) {
+           
+            await chrome.scripting.executeScript({
+              target: { tabId: tab.id },
+              files: [urlOfType === "other" ? "message.js" : "instamessage.js"],
+            });
+          }
+        }
+    
+        return; 
+      }
+    
+      if (!popupWindowId) {
+        chrome.windows.create(
+          {
+            url: `https://wobb.ai/app/discover/no-auth?username=${getSocialNameFromLink(currentUrl)}&platform=instagram`,
+            type: "popup",
+            width: width,
+            height: height,
+            left: left,
+            top: top,
+            focused: true,
+          },
+          function (newWindow) {
+            popupWindowId = newWindow.id;
+          }
+        );
+      }
+    });
+  });
+}
+
+
+function focusOrCreatePopup() {
+  if (popupWindowId) {
+    chrome.windows.get(popupWindowId, function (window) {
+      if (chrome.runtime.lastError) {
+        createPopup();
+      } else {
+        chrome.windows.update(popupWindowId, { focused: true });
+      }
+    });
+  } else {
+    createPopup();
+  }
+}
+
+
+chrome.tabs.updated.addListener(function (tabId, changeInfo, tab) {
+  if (changeInfo.status === 'complete' && tab.active) {
+    const newUrl = tab.url;
+    const changeUrl = `https://wobb.ai/app/discover/no-auth?username=${getSocialNameFromLink(newUrl)}&platform=instagram`;
+    if((changeUrl !== null || newUrl !== '') && popupWindowId !== null) {
+      if(popupWindowId) {
+        chrome.tab.query({windowId: popupWindowId}, function(tabs) {
+          if(tabs.length > 0) {
+            if(changeUrl !== instagramURL && newUrl !== null && getSocialNameFromLink(newUrl) !== 'inavalidSocialName') {
+              chrome.tabs.update(tabs[0].id, {url: `https://wobb.ai/app/discover/no-auth?username=${getSocialNameFromLink(newUrl)}&platform=instagram`})
+              instagramURL = changeUrl;
+              chrome.windows.update(popupWindowId, {focused: true})
+            }
+          }
+        })
+      }
+    }
+  }
+})
+
---
+++ message.js
@@ -0,0 +1,74 @@
+function showCustomAlert() {
+  // Create the overlay
+  const overlay = document.createElement('div');
+  overlay.style.position = 'fixed';
+  overlay.style.top = '0';
+  overlay.style.right = '0';
+  overlay.style.width = '100%';
+  overlay.style.height = '100%';
+  overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+  overlay.style.zIndex = '99999999999';
+
+  // Create the alert box
+  const alertBox = document.createElement('div');
+  alertBox.style.backgroundColor = '#fff';
+  alertBox.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.1)';
+  alertBox.style.padding = '20px';
+  alertBox.style.textAlign = 'center';
+  alertBox.style.width = '500px';
+  alertBox.style.marginLeft = 'auto';
+  alertBox.style.fontFamily = '"Poppins", sans-serif'; // Use Google Font Poppins
+
+  // Add Google Font stylesheet to the document head
+  const link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.href = 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap';
+  document.head.appendChild(link);
+
+  // Create the heading
+  const heading = document.createElement('h2');
+  heading.textContent = 'Wobb.ai';
+  heading.style.marginTop = '0';
+  heading.style.color = '#0026FF'; // Color for the heading text
+  heading.style.fontSize = '24px'; // Adjust font size if needed
+  heading.style.fontFamily = '"Poppins", sans-serif'; // Use Google Font Poppins
+
+  // Create the message
+  const message = document.createElement('p');
+  message.textContent = 'Switch to an Instagram tab, visit a user profile, and click the extension again.';
+  message.style.color = '#666';
+  message.style.width = '80%';
+  message.style.margin = 'auto';
+  message.style.fontFamily = '"Poppins", sans-serif'; // Use Google Font Poppins
+  message.style.marginTop = '20px';
+
+  // Create the close button
+  const closeButton = document.createElement('button');
+  closeButton.textContent = 'Close';
+  closeButton.style.padding = '10px 20px';
+  closeButton.style.border = 'none';
+  closeButton.style.backgroundColor = '#0026FF';
+  closeButton.style.color = '#fff';
+  closeButton.style.borderRadius = '4px';
+  closeButton.style.cursor = 'pointer';
+  closeButton.style.fontFamily = '"Poppins", sans-serif'; // Use Google Font Poppins
+  closeButton.style.marginTop = '20px';
+
+  window.messageScriptLoaded = true;
+
+  // Add event listener to close button
+  closeButton.addEventListener('click', () => {
+    document.body.removeChild(overlay);
+    window.messageScriptLoaded = false;
+  });
+
+  // Append elements
+  alertBox.appendChild(heading);
+  alertBox.appendChild(message);
+  alertBox.appendChild(closeButton);
+  overlay.appendChild(alertBox);
+  document.body.appendChild(overlay);
+}
+
+// Execute the function to show the alert
+showCustomAlert();
